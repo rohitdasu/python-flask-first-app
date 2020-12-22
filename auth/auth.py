086c68111e9a7721e0c594b823911b158b8c0dac
Rohit <dasurohit123@gmail.com>
2020-12-22 22:24:33 +0530

    added abort, request functions usage

--- auth/auth.py
+++ auth/auth.py
@@ -1,7 +1,24 @@
 from flask import Blueprint
+from flask import request, abort
 
 auth_api = Blueprint("auth_api", __name__)
 
 @auth_api.route('/')
 def auth():
     return "This is Auth API"
+
+@auth_api.route('/signin', methods=['GET', 'POST'])
+def signin():
+
+    if request.method == 'GET':
+        abort(400, "Invalid Request, Please send POST request")
+    else:
+        username = request.form['username'];
+        password = request.form['password'];
+
+        data = {
+            "username": username,
+            "password": password
+        }
+
+        return data;
