cc0a4456fdaeba57f93a4314155d21420010f038
Rohit <dasurohit123@gmail.com>
2020-12-24 11:58:12 +0530

    Added some files to deployement

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+from main import app
+import os
+
+if __name__ == '__main__':
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host='0.0.0.0', debug=True, port=port)
+
+# running flask app -> FLASK_APP=wsgi.py FLASK_ENV=development flask run
