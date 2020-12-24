a3a691e2c7a6716aa764a924c556d50c05ec3622
Rohit <dasurohit123@gmail.com>
2020-12-24 11:42:23 +0530

    Added some files to deployement

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from main import app
+import os
 
 if __name__ == '__main__':
-    app.run()
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True, port=port)
 
 # running flask app -> FLASK_APP=app.py FLASK_ENV=development flask run
