6487763e76a629065691cecee8b9c9213dcc58c8
Rohit <dasurohit123@gmail.com>
2020-12-24 11:15:06 +0530

    Added some files to deployement

--- file_upload/file.py
+++ file_upload/file.py
@@ -25,21 +25,21 @@ def allowed_file(filename):
 def upload_file():
 
     if 'file' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
+        resp = jsonify({'success': False, 'message': 'No file part in the request'})
         resp.status_code = 400
         return resp
     file = request.files['file']
     if file.filename == '':
-        resp = jsonify({'message': 'No file selected for uploading'})
+        resp = jsonify({'success': False, 'message': 'No file selected for uploading'})
         resp.status_code = 400
         return resp
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        resp = jsonify({'message': 'File successfully uploaded', 'img_url': request.host_url+'/static/assets/images/' + filename})
+        resp = jsonify({'success': True, 'message': 'File successfully uploaded', 'img_url': request.host_url + '/static/assets/images/' + filename})
         resp.status_code = 201
         return resp
     else:
-        resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp = jsonify({'success': False, 'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
         resp.status_code = 400
         return resp
