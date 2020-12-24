37a3730c2f329b3f012a1fa9441425126554b992
Rohit <dasurohit123@gmail.com>
2020-12-24 11:53:06 +0530

    Added some files to deployement

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == '__main__':
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 33507))
 
     app.run(host='0.0.0.0', debug=True, port=port)
 
